623f1ec02f6171c0122326297a51f82989330617
Sakib <kazisakib556@gmail.com>
2022-05-23 10:48:25 +0600

    onboarding page set up (some designs update)

--- lib/controllers/onboarding_controller.dart
+++ lib/controllers/onboarding_controller.dart
@@ -3,10 +3,10 @@ import 'package:to_do_list_app_flutter/models/onboarding_info.dart';
 class OnboardingController {
   List<OnboardingInfo> onboardingPages = [
     OnboardingInfo('assets/images/todo_logo1.png', 'Welcome',
-        'Now you can make your to-do list\nany time in your mobile.'),
+        'Now you can make your to-do list any time in your mobile.'),
     OnboardingInfo('assets/images/todo_logo2.png', 'Smart to-do list',
         'You can store, edit and delete your to-do list.'),
-    OnboardingInfo('assets/images/todo_logo3.png', 'Smart to-do list',
-        'You can store, edit and delete your to-do list.'),
+    OnboardingInfo('assets/images/todo_logo3.png', 'Finish your every task',
+        'Start your first to-do list.After finish every task you should select the left icon.'),
   ];
 }
--- lib/views/onboarding/onboarding_page.dart
+++ lib/views/onboarding/onboarding_page.dart
@@ -1,28 +1,54 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../../controllers/onboarding_controller.dart';
 
 class OnboardingPage extends StatelessWidget {
   OnboardingPage({Key? key}) : super(key: key);
-  final controller = OnboardingController();
+  final _controller = OnboardingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: PageView.builder(
-              itemCount: controller.onboardingPages.length,
+          body: Stack(
+        children: [
+          PageView.builder(
+              itemCount: _controller.onboardingPages.length,
               itemBuilder: (context, index) {
                 return Container(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Image.asset(controller.onboardingPages[index].imageAsset),
-                      Text(controller.onboardingPages[index].title),
-                      Text(controller.onboardingPages[index].description),
+                      Image.asset(
+                        _controller.onboardingPages[index].imageAsset,
+                        width: 140,
+                        height: 200,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        _controller.onboardingPages[index].title,
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 64.0),
+                        child: Text(
+                          _controller.onboardingPages[index].description,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
                     ],
                   ),
                 );
-              })),
+              }),
+        ],
+      )),
     );
   }
 }
